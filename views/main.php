78c6a2e98841684cf538e661ddc9e37e53d1dcbf
ScSherifTarek <sherift1552@gmail.com>
2019-06-24 17:42:27 +0200

    Adding the home url to the navbar Website name

--- views/main.php
+++ views/main.php
@@ -15,7 +15,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Shareboard</a>
+          <a class="navbar-brand" href="<?= ROOT_URL ?>">Shareboard</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
